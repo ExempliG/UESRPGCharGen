17cee3cd3bea7b0fbab4e34c8c6f1bf3a07dd758
Andrew Dysart <andrew@igglean.com>
2017-12-06 16:37:49 -0500

    Getting started on Weapon edits. Commiting/pushing for the benefit of other computers.

---
+++ UESRPG Character Manager/UI/EditWeapon.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace UESRPG_Character_Manager.UI
+{
+    partial class EditWeapon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameTb = new System.Windows.Forms.TextBox();
+            this.penNud = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.diceSidesNud = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numberOfDiceNud = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.damageModNud = new System.Windows.Forms.NumericUpDown();
+            this.enchantmentLevelNud = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.encumbranceNud = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.priceLb = new System.Windows.Forms.Label();
+            this.priceNud = new System.Windows.Forms.NumericUpDown();
+            this.direCb = new System.Windows.Forms.CheckBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.penNud)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diceSidesNud)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numberOfDiceNud)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.damageModNud)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enchantmentLevelNud)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.encumbranceNud)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.priceNud)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            // 
+            // nameTb
+            // 
+            this.nameTb.Location = new System.Drawing.Point(56, 6);
+            this.nameTb.Name = "nameTb";
+            this.nameTb.Size = new System.Drawing.Size(139, 20);
+            this.nameTb.TabIndex = 1;
+            // 
+            // penNud
+            // 
+            this.penNud.Enabled = false;
+            this.penNud.Location = new System.Drawing.Point(75, 229);
+            this.penNud.Name = "penNud";
+            this.penNud.Size = new System.Drawing.Size(38, 20);
+            this.penNud.TabIndex = 15;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 231);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "pen";
+            // 
+            // diceSidesNud
+            // 
+            this.diceSidesNud.Enabled = false;
+            this.diceSidesNud.Location = new System.Drawing.Point(75, 203);
+            this.diceSidesNud.Name = "diceSidesNud";
+            this.diceSidesNud.Size = new System.Drawing.Size(38, 20);
+            this.diceSidesNud.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 205);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "d";
+            // 
+            // numberOfDiceNud
+            // 
+            this.numberOfDiceNud.Enabled = false;
+            this.numberOfDiceNud.Location = new System.Drawing.Point(12, 203);
+            this.numberOfDiceNud.Name = "numberOfDiceNud";
+            this.numberOfDiceNud.Size = new System.Drawing.Size(38, 20);
+            this.numberOfDiceNud.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(119, 205);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "+";
+            // 
+            // damageModNud
+            // 
+            this.damageModNud.Location = new System.Drawing.Point(138, 203);
+            this.damageModNud.Name = "damageModNud";
+            this.damageModNud.Size = new System.Drawing.Size(38, 20);
+            this.damageModNud.TabIndex = 17;
+            // 
+            // enchantmentLevelNud
+            // 
+            this.enchantmentLevelNud.Location = new System.Drawing.Point(230, 6);
+            this.enchantmentLevelNud.Name = "enchantmentLevelNud";
+            this.enchantmentLevelNud.Size = new System.Drawing.Size(45, 20);
+            this.enchantmentLevelNud.TabIndex = 18;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(201, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 13);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "EL:";
+            // 
+            // encumbranceNud
+            // 
+            this.encumbranceNud.Location = new System.Drawing.Point(94, 32);
+            this.encumbranceNud.Name = "encumbranceNud";
+            this.encumbranceNud.Size = new System.Drawing.Size(61, 20);
+            this.encumbranceNud.TabIndex = 20;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 34);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 13);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Encumbrance:";
+            // 
+            // priceLb
+            // 
+            this.priceLb.AutoSize = true;
+            this.priceLb.Location = new System.Drawing.Point(12, 60);
+            this.priceLb.Name = "priceLb";
+            this.priceLb.Size = new System.Drawing.Size(34, 13);
+            this.priceLb.TabIndex = 22;
+            this.priceLb.Text = "Price:";
+            // 
+            // priceNud
+            // 
+            this.priceNud.Location = new System.Drawing.Point(94, 58);
+            this.priceNud.Name = "priceNud";
+            this.priceNud.Size = new System.Drawing.Size(61, 20);
+            this.priceNud.TabIndex = 23;
+            // 
+            // direCb
+            // 
+            this.direCb.AutoSize = true;
+            this.direCb.Location = new System.Drawing.Point(254, 233);
+            this.direCb.Name = "direCb";
+            this.direCb.Size = new System.Drawing.Size(62, 17);
+            this.direCb.TabIndex = 24;
+            this.direCb.Text = "Is Dire?";
+            this.direCb.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(15, 88);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 25;
+            this.label7.Text = "label7";
+            // 
+            // EditWeapon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 262);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.direCb);
+            this.Controls.Add(this.priceNud);
+            this.Controls.Add(this.priceLb);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.encumbranceNud);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.enchantmentLevelNud);
+            this.Controls.Add(this.damageModNud);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.penNud);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.diceSidesNud);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numberOfDiceNud);
+            this.Controls.Add(this.nameTb);
+            this.Controls.Add(this.label1);
+            this.Name = "EditWeapon";
+            this.Text = "EditWeapon";
+            ((System.ComponentModel.ISupportInitialize)(this.penNud)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diceSidesNud)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numberOfDiceNud)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.damageModNud)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enchantmentLevelNud)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.encumbranceNud)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.priceNud)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nameTb;
+        private System.Windows.Forms.NumericUpDown penNud;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown diceSidesNud;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numberOfDiceNud;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown damageModNud;
+        private System.Windows.Forms.NumericUpDown enchantmentLevelNud;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown encumbranceNud;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label priceLb;
+        private System.Windows.Forms.NumericUpDown priceNud;
+        private System.Windows.Forms.CheckBox direCb;
+        private System.Windows.Forms.Label label7;
+    }
+}
